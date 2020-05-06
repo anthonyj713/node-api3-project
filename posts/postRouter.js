8a446e0a6d4b9853a6787fdfca611f8e1a05974b
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-05-06 16:47:14 -0600

    post delete works

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -37,7 +37,24 @@ router.get('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  Posts.remove(req.params.id)
+  .then(count => {
+    if (count > 0) {
+      res.status(200).json({
+        message:'The post has been deleted'
+      })
+    } else {
+      res.status(404).json({
+        message:'The post with the specified id cannot be found.'
+      })
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      message: 'Error removing the post'
+    });
+  });
 });
 
 router.put('/:id', (req, res) => {
